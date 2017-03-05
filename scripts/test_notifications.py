6d89f7f880f0df27e1e8a2d5faab8eea45542fbd
David Christenson <mail@noctem.xyz>
2017-03-05 04:00:04 -0700

    Improvements to scripts
    Add banned accounts to banned.csv with human-readable dates and levels,
    allow all scripts to be launched from the scripts directory, add
    print_levels and print_spawns, add an expiration timestamp to
    test_notifications to fix testing with webhooks, make times in
    print_accounts human-readable, validate the CSV to make GitHub happy
    (Python doesn't care).

--- scripts/test_notifications.py
+++ scripts/test_notifications.py
@@ -1,15 +1,7 @@
 #!/usr/bin/env python3
 
-from asyncio import get_event_loop, set_event_loop_policy
+from asyncio import get_event_loop
 from pathlib import Path
-from random import uniform, randint, choice
-from argparse import ArgumentParser
-
-try:
-    from uvloop import EventLoopPolicy
-    set_event_loop_policy(EventLoopPolicy())
-except ImportError:
-    pass
 
 import time
 import logging
@@ -20,49 +12,12 @@ sys.path.append(str(monocle_dir))
 
 from monocle import names, sanitized as conf
 
-parser = ArgumentParser()
-parser.add_argument(
-    '-i', '--id',
-    type=int,
-    help='Pokémon ID to notify about'
-)
-parser.add_argument(
-    '-lat', '--latitude',
-    type=float,
-    help='latitude for fake spawn'
-)
-parser.add_argument(
-    '-lon', '--longitude',
-    type=float,
-    help='longitude for fake spawn'
-)
-parser.add_argument(
-    '-r', '--remaining',
-    type=int,
-    help='seconds remaining on fake spawn'
-)
-parser.add_argument(
-    '-u', '--unmodified',
-    action='store_true',
-    help="don't add ID to ALWAYS_NOTIFY_IDS"
-)
-args = parser.parse_args()
-
-if args.id is not None:
-    pokemon_id = args.id
-    if args.id == 0:
-        names.POKEMON_NAMES[0] = 'Test'
-else:
-    pokemon_id = randint(1, 252)
-
-if not args.unmodified:
-    conf.ALWAYS_NOTIFY_IDS = {pokemon_id}
-
+conf.ALWAYS_NOTIFY_IDS = {0}
 conf.HASHTAGS = {'test'}
+names.POKEMON_NAMES[0] = 'Test'
 
 from monocle.notification import Notifier
 from monocle.shared import SessionManager
-from monocle.names import POKEMON_MOVES
 
 root = logging.getLogger()
 root.setLevel(logging.DEBUG)
@@ -70,47 +25,28 @@ ch = logging.StreamHandler(sys.stdout)
 ch.setLevel(logging.DEBUG)
 root.addHandler(ch)
 
-MOVES = tuple(POKEMON_MOVES.keys())
-
-if args.latitude is not None:
-    lat = args.latitude
-else:
-    lat = uniform(conf.MAP_START[0], conf.MAP_END[0])
-
-if args.longitude is not None:
-    lon = args.longitude
-else:
-    lon = uniform(conf.MAP_START[1], conf.MAP_END[1])
-
-if args.remaining:
-    tth = args.remaining
-else:
-    tth = uniform(89, 3599)
-
-now = time.time()
-
 pokemon = {
     'encounter_id': 93253523,
     'spawn_id': 3502935,
-    'pokemon_id': pokemon_id,
-    'time_till_hidden': tth,
-    'lat': lat,
-    'lon': lon,
-    'individual_attack': randint(0, 15),
-    'individual_defense': randint(0, 15),
-    'individual_stamina': randint(0, 15),
-    'seen': now,
-    'move_1': choice(MOVES),
-    'move_2': choice(MOVES),
+    'pokemon_id': 0,
+    'time_till_hidden': 89,
+    'lat': 40.776714,
+    'lon': -111.888558,
+    'individual_attack': 15,
+    'individual_defense': 15,
+    'individual_stamina': 15,
+    'seen': time.time(),
+    'move_1': 13,
+    'move_2': 14,
     'valid': True,
-    'expire_timestamp': now + tth
+    'expire_timestamp': time.time() + 89
 }
 
 notifier = Notifier()
 
 loop = get_event_loop()
 
-if loop.run_until_complete(notifier.notify(pokemon, randint(1, 2))):
+if loop.run_until_complete(notifier.notify(pokemon, 2)):
     print('Success')
 else:
     print('Failure')
